16c9ee102a6a61a359d2933fdbd6afa4b962997e
Ramesh Vaidyanathan <ramesh.vaidyanathan@avanade.com>
2021-05-13 20:21:17 -0400

    removed reference to next image

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+//import Image from 'next/image'
 import styles from '../styles/layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
